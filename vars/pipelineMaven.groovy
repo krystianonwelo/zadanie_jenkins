5fe61628d7cfb94c7f72178204feb9ef8f5f8ca6
Krystian Wieczorek <krystianw@LLLUPF3MHD8Q.onwelo.net>
2023-06-05 12:27:31 +0200

    test12

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -1,11 +1,10 @@
-env = [MODULE_NAME: 'działa', ENVIRONMENT: 'bar', REPO_NAME: 'baz']
 
 def call(Map config = [:]) {
-    defaultMap = [moduleName: env.MODULE_NAME, environment: env.ENVIRONMENT, repoName: env.REPO_NAME]
+    defaultMap = [moduleName: 'działa', environment: 'bar', repoName: 'baz']
     config = defaultMap << config
     
     stage('demo') {
-        echo ${config.MODULE_NAME}
+        echo ${config.moduleName}
     }
 }
 
