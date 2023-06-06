50766cba583b1c335bb380cbf365612b8a9fcfc3
Krystian Wieczorek <krystianw@LLLUPF3MHD8Q.onwelo.net>
2023-06-06 13:31:25 +0200

    commit elo32O

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -2,9 +2,30 @@
 def call(Map config = [:]) {
     defaultMap = [moduleName: 'działa', environment: 'bar', repoName: 'baz']
     config = defaultMap << config
-    
-    stage('demo') {
-        echo "${config.moduleName}"
+
+    node {
+        stage('Downloading Source Code') {
+            steps{
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[ url: 'https://github.com/krystianonwelo/spring-petclinic.git']]])
+            }
+        }
+        stage('Building Source Code') {
+            steps {
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn verify'
+            }
+            post {
+                always {
+                    junit 'target/surefire-reports/*.xml'
+                }
+            }
+        }
+        stage('Installing Artifacts') {
+            sh 'mvn install -DskipTests'
+        }
     }
 }
-
